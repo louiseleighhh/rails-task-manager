1d43445df090604f6f40ffc34956e0650fa83b9b
louiseleighhh <louisecapala@gmail.com>
2022-07-04 00:09:50 +0900

    refactored it

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -17,18 +17,14 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -37,5 +33,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # Read all the tasks
-  get 'tasks', to: 'tasks#index', as: 'tasks'
-  # Create a task
-  get 'tasks/new', to: 'tasks#new', as: 'task_new'
-  post 'tasks', to: 'tasks#create'
-  # Read one task
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-  # Update
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
-  patch 'tasks/:id', to: 'tasks#update'
-  # Delete
-  delete 'tasks/:id', to: 'tasks#destroy', as: 'task_destroy'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # Read all the tasks
+  # get 'tasks', to: 'tasks#index', as: 'tasks'
+  # # Create a task
+  # get 'tasks/new', to: 'tasks#new', as: 'task_new'
+  # post 'tasks', to: 'tasks#create'
+  # # Read one task
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # # Update
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy', as: 'task_destroy'
+  resources :tasks
 end
